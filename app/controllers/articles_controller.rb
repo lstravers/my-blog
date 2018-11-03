c82f723197e48dc4faf4950b00e53920a968ae10
lstravers <stravers222@gmail.com>
2018-11-02 21:19:09 -0400

    add validations to article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,22 +12,28 @@ def show
 end
 
 def new
+  @article = Article.new
 end
 
 def create
   @article = Article.new(article_params)
     #The capital "A" about referrs to the class named Article that is defined in app/models/article.rb. Class names in Ruby must begin with a capital letter.
 
-  @article.save 
+  if @article.save 
   #returns a boolean indicating whether the article was saved or not
   redirect_to @article
+  else
+    render 'new'
+  end
 end
+
+#use render instead of redirect_to when save returns false. The render method is used so that the @article object is passed back to the new template when it is rendered. This rendering is done within the same request as the form submission, whereas the redirect_to will tell the browser to issue another request.
 
 #strong parameters: whitelist  controller parameters to prevent wrongful mass assignment. In this case, we want to both allow and require the title and text parameters for valid use of create
 
 private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :description)
   end
 
 end
