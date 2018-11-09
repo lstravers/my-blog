5128754e348f87c28b759c69ded9d3b890a0385c
lstravers <stravers222@gmail.com>
2018-11-09 14:28:11 -0500

    add comment function and styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   #tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action.
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
   #tells Rails to map requests to the root of the application to the welcome controller's index action
